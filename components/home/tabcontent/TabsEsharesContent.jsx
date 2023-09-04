0a7bcfe0a28d69d5385c8017b3fa42b2bb13ddfc
jaysoncano <cano.jayson10@gmail.com>
2023-09-04 11:43:00 +0800

    add chart

--- components/home/tabcontent/TabsEsharesContent.jsx
+++ components/home/tabcontent/TabsEsharesContent.jsx
@@ -1,5 +1,31 @@
+"use client"
 import { FaSquarePollVertical, FaChartLine } from "react-icons/fa6"
+import { useEffect } from "react"
+import Chart from 'chart.js/auto';
 const TabsEsharesContent = () => {
+  useEffect(() => {
+    var ctx = document.getElementById('myChart');
+    var myChart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        labels: [
+          'Red',
+          'Blue',
+          'Yellow'
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [300, 50, 100],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+        }]
+      }
+    });
+  }, [])
   return (
     <>
       <div className="row">
@@ -80,8 +106,10 @@ const TabsEsharesContent = () => {
               </div>
             </div>
             <div className="m-portlet__body">
-              <div className="relative preloader-notch">
-                insert chart here
+              <div className="tw-min-h-[80vh]">
+              <canvas id='myChart'></canvas>
+              </div>
+              <div className="relative preloader-notch !tw-hidden">
               </div>
             </div>
           </div>
